f126e78fdce9136a9d17914253bc95b2b21b7954
Luiz Gustavo Freire Gama <luizgamabh@gmail.com>
2015-03-17 13:38:20 -0300

    inital structure

---
+++ app/index.js
@@ -0,0 +1,91 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+    this.argument('appname', {type: String, required: true});
+    this.appname = this._.camelize(this.appname);
+  },
+
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('custom') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'customGsOption',
+      message: 'Would you like to enable custom grid system?',
+      default: true,
+      store: true // saves the answer
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.customGsOption = props.customGsOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+
+      this.mkdir('resources');
+      this.mkdir('resources/sass');
+      this.mkdir('resources/js');
+      this.mkdir('resources/copy');
+      this.mkdir('resources/font');
+      this.mkdir('resources/html');
+      this.mkdir('resources/img');
+      this.mkdir('resources/sprite');
+
+      this.fs.directory(
+        this.templatePath('h5bp/sass'),
+        this.destinationPath('resources/sass')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('_compass-dev.rb'),
+        this.destinationPath('compass-dev.rb')
+      );
+      this.fs.copy(
+        this.templatePath('_compass-dist.rb'),
+        this.destinationPath('compass-dist.rb')
+      );
+      this.fs.copy(
+        this.templatePath('h5bp/.editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  }
+});
